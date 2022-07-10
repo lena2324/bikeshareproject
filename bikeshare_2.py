e09ef5a4104c2fecbebf76f1e6c32415fed637f5
lena2324 <109045094+lena2324@users.noreply.github.com>
2022-07-10 17:06:44 -0500

    Update bikeshare_2.py

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -55,16 +55,15 @@ print('-' * 40)
 
 def load_data(city, month, day):
     return df   #I had to move df here because otherwise it was giving me an error that it's outside of the range
-   #
-    #Loads data for the specified city and filters by month and day if applicable.
-#
+   
+    #Loads data for the specified city and filters by month and day if applicable
     #Args:
        # (str) city - name of the city to analyze
         #(str) month - name of the month to filter by, or "all" to apply no month filter
        # (str) day - name of the day of week to filter by, or "all" to apply no day filter
    # Returns:
         #df - Pandas DataFrame containing city data filtered by month and day
-    #
+   
 
     # load data file into a dataframe
 print("Loading data...")
